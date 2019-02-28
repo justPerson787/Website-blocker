d1cabfec39a3410f7c55e475b7948187a581c4d8
Natalia-Ushakova <n.n.ushakova@gmail.com>
2019-02-28 00:03:42 -0500

    Initial commit

---
+++ website-blocker.py
@@ -0,0 +1,5 @@
+import time
+from datetime import datetime as dt
+hosts_path = 'C:\Windows\System32\Drivers\etc\hosts' #access to hosts file
+redirect = '127.0.0.1' #where browser will be redirected
+website_list = ['www.facebook.com', 'facebook.com', 'www.1tv.ru']#websites to block
